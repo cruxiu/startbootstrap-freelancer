401be22617009d3cdfbae0228d5b49052fe78335
Luís Eduardo <luis.eduardo.225@gmail.com>
2017-09-26 14:18:52 -0300

    site-mp-manutencoes: projeto base.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -6,21 +6,22 @@ if(empty($_POST['name'])  		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
+	echo "Nenhum dado fornecido!";
 	return false;
    }
-	
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
+$company = strip_tags(htmlspecialchars($_POST['company']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-	
+
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$to = 'contato@mpmanutencoes.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Formulario de contato do site:  $name";
+$email_body = "Voce recebeu uma nova mensagem do formulario de contato do seu site.\n\n"."Aqui estao os detalhes:\n\nNome: $name\n\nEmpresa: $company\n\nEmail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
+$headers = "From: naoresponder@mpmanutencoes.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Responder a: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+return true;
 ?>
